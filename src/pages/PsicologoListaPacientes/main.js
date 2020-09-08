ed9b8581a2c6e59ae52dc8ca1ee61a021f1cf419
Andre Chimicatti Fabrini <C0007186@vli-logistica.com.br>
2020-09-07 21:38:29 -0300

    tela lista de pacientes parte funcional finalizada

--- src/pages/PsicologoListaPacientes/main.js
+++ src/pages/PsicologoListaPacientes/main.js
@@ -1,38 +1,35 @@
 listarPacientes(mockListaPacientes);
 
-function listarPacientes(mockListaPacientes) {
-
-    console.log("chamando listarPacientes");
+function listarPacientes(ListaPacientes, diaMarcadoPeloPsicologo) {
 
     let listaPacientes = document.getElementById("lista_pacientes");
-    listaPacientes.innerHTML = "";
+    // listaPacientes.innerHTML = "";
 
-    mockListaPacientes.forEach(paciente => {
-
-        console.log(paciente);
+    ListaPacientes.forEach(paciente => {
 
         let ultimaEmocao = paciente.ultimaEmocao;
         let nome = paciente.nome;
         let matricula = paciente.matricula;
         let foto = paciente.foto;
-        let diaConsulta = paciente.diaConsulta;
+        let diaConsulta = diaMarcadoPeloPsicologo || paciente.diaConsulta;
 
-        let itemListaString = itemListaConstructor(ultimaEmocao, nome, foto, diaConsulta);
+        let itemListaString = itemListaConstructor(matricula, ultimaEmocao, nome, foto, diaConsulta);
             
         let itemLista = document.createElement('div');
         itemLista.className = 'item_lista_container';
-        itemLista.id = matricula;
         itemLista.innerHTML = itemListaString;
 
         listaPacientes.appendChild(itemLista);
     })
 }
 
-function itemListaConstructor(ultimaEmocao, nome, foto, diaConsulta) {
+function exibeDadosPaciente(matricula){
+    alert("fazer requisição para o paciente " + matricula);
+}
 
-
+function itemListaConstructor(matricula, ultimaEmocao, nome, foto, diaConsulta) {
     var itemLista =
-        `
+        `<div class="item_lista_overlay" id="${matricula}" onclick="exibeDadosPaciente(this.id)"></div>
         <div class="indicador_emocao ${definirEmocao(ultimaEmocao)}"></div> \
         <img src="${foto}" class="foto_paciente_lista"> \
         <p class="nome_paciente_lista">${nome}</p> \
@@ -67,14 +64,9 @@ function listarDiasDaSemana(diaConsulta) {
         ["sábado", "Sáb"]
     ]
 
-    console.log(diasDaSemana);
-
     let semana = "";
 
     for (d = 0; d <= 5; d++) {
-
-        console.log(diasDaSemana[d]);
-
         let diaSemana = 
             `<div class="dia_semana_consulta">
                 <div class="dia_semana 
@@ -89,6 +81,46 @@ function listarDiasDaSemana(diaConsulta) {
         semana += diaSemana;
     }
         
-    // agendaSemanal.innerHTML = semana;
     return semana;
+}
+
+function cadastrarNovoPaciente() {
+    
+    var matriculaAdicionada = document.getElementById("matricula_cadastro").value;
+    var diaConsulta = document.getElementById("dia_consulta_cadastro").value;
+    var feedbackCadastro = document.getElementById("feedback_cadastro");
+
+    var paciente = mockListaPacientesParaAdicionar.filter(p => p.matricula === parseInt(matriculaAdicionada));
+
+    paciente.length > 0 ?
+    feedbackCadastro.innerHTML = "Paciente Cadastrado com Sucesso!" :
+    feedbackCadastro.innerHTML = "Paciente Não Encontrado";
+
+    listarPacientes(paciente, diaConsulta);
+}
+
+function abrirModalCadastro() {
+    var modalCadastro = document.getElementById("modal_adicionar_paciente");
+    var overlayModal = document.getElementById("modal_cadastro_overlay");
+
+    modalCadastro.style.display = "block";
+    overlayModal.style.display = "block";
+}
+
+function fecharModalCadastro() {
+    var modalCadastro = document.getElementById("modal_adicionar_paciente");
+    var overlayModal = document.getElementById("modal_cadastro_overlay");
+
+    modalCadastro.style.display = "none";
+    overlayModal.style.display = "none";
+
+    limparModalCadastro();
+}
+
+function limparModalCadastro() {
+
+    var matriculaAdicionada = document.getElementById("matricula_cadastro").value = "";
+    var diaConsulta = document.getElementById("dia_consulta_cadastro").value = "segunda-feira";
+    var feedbackCadastro = document.getElementById("feedback_cadastro").innerHTML = "";
+
 }
